def6ec250efd42c9bf73b33c48efbda453939442
avinitzca <avinitzca@gmail.com>
2021-08-17 19:30:23 -0300

    Add project files.

---
+++ TGC.MonoGame.TPZero/FollowCamera.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TGC.MonoGame.TP
+{
+    /// <summary>
+    /// A Camera that follows objects
+    /// </summary>
+    class FollowCamera
+    {
+        private const float AxisDistanceToTarget = 1000f;
+
+        private const float AngleFollowSpeed = 0.015f;
+
+        private const float AngleThreshold = 0.85f;
+
+        public Matrix Projection { get; private set; }
+
+        public Matrix View { get; private set; }
+
+        private Vector3 CurrentRightVector { get; set; } = Vector3.Right;
+
+        private float RightVectorInterpolator { get; set; } = 0f;
+
+        private Vector3 PastRightVector { get; set; } = Vector3.Right;
+
+        /// <summary>
+        /// Creates a FollowCamera to follow a specific World matrix
+        /// </summary>
+        /// <param name="aspectRatio"></param>
+        public FollowCamera(float aspectRatio)
+        {
+            // Orthographic camera
+            // Projection = Matrix.CreateOrthographic(screenWidth, screenHeight, 0.01f, 10000f);
+
+            // Perspective camera
+            // Use 60° as FOV, the aspect ratio, set 0.1 as near plane and 100000 (a lot) as a far plane distance
+            Projection = Matrix.CreatePerspectiveFieldOfView(MathF.PI / 3f, aspectRatio, 0.1f, 100000f);
+        }
+
+        /// <summary>
+        /// Updates the Camera using an updated World matrix to follow
+        /// </summary>
+        /// <param name="gameTime">The Game Time to calculate framerate-independent movement</param>
+        /// <param name="followedWorld">The World matrix to follow</param>
+        public void Update(GameTime gameTime, Matrix followedWorld)
+        {
+            // Extract the Elapsed Time
+            var elapsedTime = Convert.ToSingle(gameTime.ElapsedGameTime.TotalSeconds);
+
+            // Get the position of the followed world matrix
+            var followedPosition = followedWorld.Translation;
+
+            // Get the Right vector from the followed world matrix
+            var followedRight = followedWorld.Right;
+
+            // If the dot product of the past right vector
+            // and the current right vector is greater than a threshold,
+            // move the Interpolator variable (from 0 to 1) closer to one
+            if (Vector3.Dot(followedRight, PastRightVector) > AngleThreshold)
+            {
+                // Add an increment to Interpolator
+                RightVectorInterpolator += elapsedTime * AngleFollowSpeed;
+
+                // Clamp Interpolator to 1 maximum
+                RightVectorInterpolator = MathF.Min(RightVectorInterpolator, 1f);
+
+                // Calculate the right vector from the interpolation
+                // This moves the Right vector to match the followed Right vector
+                // In this case use a x^2 curve to make it smoother
+                // Interpolator will eventually become 1
+                CurrentRightVector = Vector3.Lerp(CurrentRightVector, followedRight, RightVectorInterpolator * RightVectorInterpolator);
+            }
+            else
+                // If the angle does not pass the threshold, set it back to zero
+                RightVectorInterpolator = 0f;
+
+            // Store the Right vector to use it the next iteration
+            PastRightVector = followedRight;
+            
+            // Calculate the camera position
+            // Take the followed position, add an offset in the Y and Followed Right axis
+            var offsetedPosition = followedPosition 
+                + CurrentRightVector * AxisDistanceToTarget
+                + Vector3.Up * AxisDistanceToTarget;
+
+            // Calculate the updated Up vector
+            // Note that the default Up vector (0, 1, 0) can't be used as 
+            // it is not correct, calculate the correct up vector instead
+            // (by using this cross-product trick)
+
+            // Calculate the Forward vector by subtracting the destination to the origin
+            // Then normalizing it (expensive!)
+            // (This operation needs normalized vectors)
+            var forward = (followedPosition - offsetedPosition);
+            forward.Normalize();
+
+            // Get the right vector by assuming the camera has the Up vector pointing upwards
+            // and it is not rotated in the X axis (roll)
+            var right = Vector3.Cross(forward, Vector3.Up);
+
+            // Once the correct Camera Right vector is calculated, get the correct Camera Up vector by using 
+            // another cross product
+            var cameraCorrectUp = Vector3.Cross(right, forward);
+
+            // Calculate the View matrix by using the Camera Position, the Position the Camera is looking at,
+            // and the Camera Up vector
+            View = Matrix.CreateLookAt(offsetedPosition, followedPosition, cameraCorrectUp);
+        }
+    }
+}
---
+++ TGC.MonoGame.TPZero/TGCGame.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using TGC.MonoGame.TP.Content.Models;
+
+namespace TGC.MonoGame.TP
+{
+    /// <summary>
+    ///     Main class of the Game.
+    /// </summary>
+    public class TGCGame : Game
+    {
+        public const string ContentFolder3D = "Models/";
+        public const string ContentFolderEffects = "Effects/";
+        public const string ContentFolderMusic = "Music/";
+        public const string ContentFolderSounds = "Sounds/";
+        public const string ContentFolderSpriteFonts = "SpriteFonts/";
+        public const string ContentFolderTextures = "Textures/";
+        
+        private GraphicsDeviceManager Graphics { get; }
+        private CityScene City { get; set; }
+        private Model CarModel { get; set; }
+        private Matrix CarWorld { get; set; }
+        private FollowCamera FollowCamera { get; set; }
+
+
+        /// <summary>
+        ///     Game constructor
+        /// </summary>
+        public TGCGame()
+        {
+            // Handles the config and administration of the Graphics Device
+            Graphics = new GraphicsDeviceManager(this);
+
+            // Root folder where resources are located
+            Content.RootDirectory = "Content";
+
+            // Makes the mouse visible
+            IsMouseVisible = true;
+        }
+
+        /// <summary>
+        ///     Called once at the initialization of the application.
+        ///     Write here the code about initialization: everything that can be pre-calculated for the application.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // Turn on Back-Face culling
+            // Also set Blend State to opaque
+            var rasterizerState = new RasterizerState();
+            rasterizerState.CullMode = CullMode.CullCounterClockwiseFace;
+            GraphicsDevice.RasterizerState = rasterizerState;
+            GraphicsDevice.BlendState = BlendState.Opaque;
+
+            // Set the window size
+            Graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width - 100;
+            Graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height - 100;
+            Graphics.ApplyChanges();
+
+            // Create a camera to follow our car
+            FollowCamera = new FollowCamera(GraphicsDevice.Viewport.AspectRatio);
+
+            // Configure the Car World matrix
+            CarWorld = Matrix.Identity;
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        ///     Called once at the initialization of the application, after Initialize, and once the GraphicsDevice is set.
+        ///     Should be used for resource and content loading.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create the City Scene
+            City = new CityScene(Content);
+
+            // Content loading should be done here
+
+            base.LoadContent();
+        }
+
+        /// <summary>
+        ///     Called N times each second. Generally 60 times, but can be configured.
+        ///     The main logic should be written here, including processing inputs.
+        /// </summary>
+        protected override void Update(GameTime gameTime)
+        {
+            // Capture the keyboard state
+            var keyboardState = Keyboard.GetState();
+            if (keyboardState.IsKeyDown(Keys.Escape))
+                // Exit the game
+                Exit();
+
+            // Update logic should go here
+
+            // Update the camera, passing the World matrix of the car
+            FollowCamera.Update(gameTime, CarWorld);
+
+            base.Update(gameTime);
+        }
+
+
+        /// <summary>
+        ///     Called every frame.
+        ///     Draw logic should be written here.
+        /// </summary>
+        protected override void Draw(GameTime gameTime)
+        {
+            // Clear the screen
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // Draw the city
+            City.Draw(gameTime, FollowCamera.View, FollowCamera.Projection);
+
+            base.Draw(gameTime);
+        }
+
+        /// <summary>
+        ///     Free the loaded resources.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // Free the resources loaded by the Content Manager
+            Content.Unload();
+
+            base.UnloadContent();
+        }
+    }
+}
