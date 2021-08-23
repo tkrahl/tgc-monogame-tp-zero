e512636682c3ee0c02fe86fb8aeffb8854956243
leandroosuna <leandrog.osuna@gmail.com>
2021-08-23 15:22:58 -0300

    switch comments to spanish

--- TGC.MonoGame.TPZero/TGCGame.cs
+++ TGC.MonoGame.TPZero/TGCGame.cs
@@ -8,7 +8,7 @@ using TGC.MonoGame.TP.Content.Models;
 namespace TGC.MonoGame.TP
 {
     /// <summary>
-    ///     Main class of the Game.
+    ///     Clase principal del juego.
     /// </summary>
     public class TGCGame : Game
     {
@@ -27,76 +27,76 @@ namespace TGC.MonoGame.TP
 
 
         /// <summary>
-        ///     Game constructor
+        ///     Constructor del juego
         /// </summary>
         public TGCGame()
         {
-            // Handles the config and administration of the Graphics Device
+            // Se encarga de la configuracion y administracion del Graphics Device
             Graphics = new GraphicsDeviceManager(this);
 
-            // Root folder where resources are located
+            // Carpeta donde estan los recursos que vamos a usar
             Content.RootDirectory = "Content";
 
-            // Makes the mouse visible
+            // Hace que el mouse sea visible
             IsMouseVisible = true;
         }
 
         /// <summary>
-        ///     Called once at the initialization of the application.
-        ///     Write here the code about initialization: everything that can be pre-calculated for the application.
+        ///     Llamada una vez en la inicializacion de la aplicacion.
+        ///     Escribir aca todo el codigo de inicializacion: Todo lo que debe estar precalculado para la aplicacion.
         /// </summary>
         protected override void Initialize()
         {
-            // Turn on Back-Face culling
-            // Also set Blend State to opaque
+            // Enciendo Back-Face culling
+            // Configuro Blend State a Opaco
             var rasterizerState = new RasterizerState();
             rasterizerState.CullMode = CullMode.CullCounterClockwiseFace;
             GraphicsDevice.RasterizerState = rasterizerState;
             GraphicsDevice.BlendState = BlendState.Opaque;
 
-            // Set the window size
+            // Configuro el tamaño de la pantalla
             Graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width - 100;
             Graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height - 100;
             Graphics.ApplyChanges();
 
-            // Create a camera to follow our car
+            // Creo una camaar para seguir a nuestro auto
             FollowCamera = new FollowCamera(GraphicsDevice.Viewport.AspectRatio);
 
-            // Configure the Car World matrix
+            // Configuro la matriz de mundo del auto
             CarWorld = Matrix.Identity;
 
             base.Initialize();
         }
 
         /// <summary>
-        ///     Called once at the initialization of the application, after Initialize, and once the GraphicsDevice is set.
-        ///     Should be used for resource and content loading.
+        ///     Llamada una sola vez durante la inicializacion de la aplicacion, luego de Initialize, y una vez que fue configurado GraphicsDevice.
+        ///     Debe ser usada para cargar los recursos y otros elementos del contenido.
         /// </summary>
         protected override void LoadContent()
         {
-            // Create the City Scene
+            // Creo la escena de la ciudad
             City = new CityScene(Content);
 
-            // Content loading should be done here
+            // La carga de contenido debe ser realizada aca
 
             base.LoadContent();
         }
 
         /// <summary>
-        ///     Called N times each second. Generally 60 times, but can be configured.
-        ///     The main logic should be written here, including processing inputs.
+        ///     Es llamada N veces por segundo. Generalmente 60 veces pero puede ser configurado.
+        ///     La logica general debe ser escrita aca, junto al procesamiento de mouse/teclas
         /// </summary>
         protected override void Update(GameTime gameTime)
         {
-            // Capture the keyboard state
+            // Caputo el estado del teclado
             var keyboardState = Keyboard.GetState();
             if (keyboardState.IsKeyDown(Keys.Escape))
-                // Exit the game
+                // Salgo del juego
                 Exit();
 
-            // Update logic should go here
+            // La logica debe ir aca
 
-            // Update the camera, passing the World matrix of the car
+            // Actualizo la camara, enviandole la matriz de mundo del auto
             FollowCamera.Update(gameTime, CarWorld);
 
             base.Update(gameTime);
@@ -104,26 +104,28 @@ namespace TGC.MonoGame.TP
 
 
         /// <summary>
-        ///     Called every frame.
-        ///     Draw logic should be written here.
+        ///     Llamada para cada frame
+        ///     La logica de dibujo debe ir aca.
         /// </summary>
         protected override void Draw(GameTime gameTime)
         {
-            // Clear the screen
+            // Limpio la pantalla
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // Draw the city
+            // Dibujo la ciudad
             City.Draw(gameTime, FollowCamera.View, FollowCamera.Projection);
+
+            // El dibujo del auto debe ir aca
 
             base.Draw(gameTime);
         }
 
         /// <summary>
-        ///     Free the loaded resources.
+        ///     Libero los recursos cargados
         /// </summary>
         protected override void UnloadContent()
         {
-            // Free the resources loaded by the Content Manager
+            // Libero los recursos cargados dessde Content Manager
             Content.Unload();
 
             base.UnloadContent();
